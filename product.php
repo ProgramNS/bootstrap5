31c5651aba9a7598c7be9ba1d9f75b162cfd1f12
ProgramNS <blise01@hotmail.com>
2022-07-20 21:56:38 +0700

    Update

--- product.php
+++ product.php
@@ -2,7 +2,7 @@
     require_once __DIR__ . './src/cdnBootstrap.php';
     require_once __DIR__ . './src/cdnFontAwesome.php';
     require_once __DIR__ . './src/navbar.php';
-    require_once __DIR__ . './modal.php';
+    require_once __DIR__ . './database/tambahData.php';
     require_once __DIR__ . './database/getConnection.php';          
     ?>
 
@@ -19,13 +19,14 @@
     <button type="button" class="btn btn-primary mt-5 ms-4" data-bs-toggle="modal" data-bs-target="#exampleModal">Tambah Data</button>
     <!-- Tabel Produk -->
     <div class="container-fluid w-100 bg-dark bg-opacity-10 mt-4 mb-2">
-      <table class="table">
+      <table class="table table-hover">
         <thead>
           <tr>
             <th scope="col">No</th>
             <th scope="col">Brand</th>
             <th scope="col">Ukuran</th>
             <th scope="col">Kondisi</th>
+            <th scope="col">Harga</th>
             <th scope="col">Foto</th>
             <th scope="col">Action</th>
           </tr>
@@ -36,17 +37,18 @@
           $conncection = getConnection();
           $sql = $conncection -> prepare("select * from tb_product"); 
           $sql -> execute(); 
-          $data = $sql -> fetch();
-          while($data) {
+          while($data = $sql -> fetch()) {
           ?>
             <tr>
-              <td><?php echo $data['id'];?></td>
+              <th scope="row"><?php echo $data['id'];?></th>
               <td><?php echo $data['brand'];?></td>
               <td><?php echo $data['ukuran'];?></td>
               <td><?php echo $data['kondisi_topi'];?></td>
+              <td><?php echo $data['harga'];?></td>
               <td><img src='upload/<?php echo $data['foto'];?>'style='width:40px;'/></td>
-              <td><a href='#'><button class='btn btn-primary btn-sm'>Edit</button></td></a>
-            </tr>
+              <td><a href='#'><button class='btn btn-primary btn-sm'>Edit</button>
+                  <a href='#'><button class='btn btn-primary btn-sm'>Delete</button></td></a>
+              </tr>
             <?php }?>
             <?php $conncection = null;?>
           </tbody>
